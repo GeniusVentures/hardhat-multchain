e7c51af42566cd18e306a1b9e381ab523ba18783
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2025-07-10 18:16:05 -0400

    node module prep

--- dist/test/helpers.js
+++ dist/test/helpers.js
@@ -3,8 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.useEnvironment = useEnvironment;
-exports.getHre = getHre;
+exports.getHre = exports.useEnvironment = void 0;
 const plugins_testing_1 = require("hardhat/plugins-testing");
 const path_1 = __importDefault(require("path"));
 // Global variable to store the HRE for tests
@@ -20,7 +19,9 @@ function useEnvironment(fixtureProjectName) {
     });
     return hre;
 }
+exports.useEnvironment = useEnvironment;
 function getHre() {
     return hre;
 }
+exports.getHre = getHre;
 //# sourceMappingURL=helpers.js.map
