19e133d6268127fe19910b900b2c58b04101d2cd
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2025-07-10 22:04:46 -0400

    fixing dist/src

--- dist/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { JsonRpcProvider } from "@ethersproject/providers";
-import { ChainConfigError, NetworkConnectionError, ProcessCleanupError } from "./chainManager";
-import { MultiChainProviders } from "./type-extensions";
-export declare function getProvider(networkName: string): JsonRpcProvider;
-export declare function getMultichainProviders(): MultiChainProviders;
-export { ChainConfigError, NetworkConnectionError, ProcessCleanupError };
-export { default as multichain } from "./chainManager";
-export {};
-//# sourceMappingURL=index.d.ts.map
